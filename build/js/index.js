bbfa0f28738a43eefc2bbbbe47ae7af5656e6977
deadka1313 <deadka1313@gmail.com>
2018-01-16 05:14:36 +0700

    Теперь css build в отдельной папке а не инлайном в html. javaScript файлы при сборке помещатся в js/

--- build/js/index.js
+++ build/js/index.js
@@ -60,14 +60,24 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 3);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 1 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__menu_styl__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__menu_styl___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__menu_styl__);
+
+
 /* harmony default export */ __webpack_exports__["a"] = (function (array, className) {
   var menu = document.createElement("ul");
   menu.className = className;
@@ -80,17 +90,36 @@
 });
 
 /***/ }),
-/* 1 */
+/* 2 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 3 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__components_menu_menu__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__index_styl__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__index_styl___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__index_styl__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_normalize_css__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_normalize_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_normalize_css__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__components_menu_menu__ = __webpack_require__(1);
 
-var menu =Object(__WEBPACK_IMPORTED_MODULE_0__components_menu_menu__["a" /* default */])(['Главная','Блог'], 'menu');
+
+
+
+var menu =Object(__WEBPACK_IMPORTED_MODULE_2__components_menu_menu__["a" /* default */])(['Главная','Блог'], 'menu');
 document.body.appendChild(menu);
 
 console.log('in index.js');
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
